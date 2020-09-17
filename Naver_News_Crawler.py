77a895044bba6ccfe5fdcb69c1242bba38b797f7
jyno <jinho9613@naver.com>
2020-09-18 01:42:32 +0900

    crawling data select

--- Naver_News_Crawler.py
+++ Naver_News_Crawler.py
@@ -1,62 +1,43 @@
-from bs4 import BeautifulSoup
-import urllib.request as req
-import pandas as pd
-import csv
-# 몇 페이지 크롤링 할지 정해두고
-# 각 뉴스는 1부터 10페이지까지의 헤드라인을 긁어온다.
-# 그리고 각 헤드라인에 맞는 세션에 저장한다.
+import requests as req
+import bs4 as soup
 
-# 정치일반 : https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=269&sid1=100&date=20200511&page=
-# 경제일반 : https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=263&sid1=101&date=20200511&page=
-# 사회일반 : https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=257&sid1=102&date=20200511&page=
-# 생활일반 : https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=245&sid1=103&date=20200511&page=
-# 세계일반 : https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=332&sid1=104&date=20200511&page=
+# input
+# required / Not required
+# word / sort, ds, de
+# 검색할 단어/ 정렬 조건, 시작일, 마지막 일
 
-# csvfile = csv.writer(file)
+"""
+    기간 / pd값
+    전체 0
+    1일 4
+    1주 1 
+    1개월 2
+    6개월 6
+    1년 5 
+    1시간 7 
+    2시간 8 
+    3시간 9 
+    4시간 10
+    5시간 11
+    6시간 12
 
-# csv 파일을 만드든 코드
-page = []
-date = []
-for i in range(1, 21):
-    page.append(i)
-session = [(100, 269, "정치"), (101, 263, "경제"), (102, 257, "사회"),
-           (103, 245, "생활"), (104, 332, "세계"), ]
-# 몇 페이지 크롤링할까 설정
-for i in range(20200413, 20200418):
-    date.append(i)
- # file = open(i[2]+".txt", 'w', encoding='utf-8-sig', newline='')
- # 처음 저장방식을 txt로 하려고 했었던 시도.
-file = open("new_test_10day.csv", 'a', encoding='utf-8-sig', newline='')
-# newtrain은 20200504부터 0516
-# newtest는  20200406 부터 0418
-file.write("제목"+',')
-file.write("세션"+'\n')
-resultList = []
-for d in date:
-    print("현재 진행된 날짜 : ", d)
-    # 날짜만큼 반복 크롤링
-    for i in session:
-        # 세션마다 크롤링
-        for temp in page:
-            # 페이지마다 크롤링
-            url = "https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2="+str(i[1])+"&sid1="+str(i[0])+"&date="+str(d)+"&page=" + \
-                str(temp)
-            res = req.urlopen(url)
-            soup = BeautifulSoup(res, "html.parser")
-            title_list = soup.select(
-                "div.list_body > ul > li > dl > dt:nth-of-type(2) > a ")
+    custom 3 (없어도 작동함)
+    기간을 지정하고 pd를 3 이외의 것으로 두면 pd값이 우선임
+    (ex 2010.03.01 을 시작날로 두고 pd를 4로 주면 날자는 사라지고 pd4만 쿼리에 들어가고 오늘부터 1일전 기사만 출력됨)
 
-            for t in title_list:
-                data = t.text.strip().replace(",", "")
-                # csv에서 ,로 구분하는데 뉴스 제목에 ',' 때문에 저장방식에 오류 발생 - ',' 삭제
-                resultList += [(data, str(i[2]))]
+"""
+url = "https://search.naver.com/search.naver?where=news&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sm=tab_srt&sort=1&ds=2020.09.09&de=2020.09.16"
+# https://search.naver.com/search.naver?where=news
+# &query=(검색어)&sm=tab_srt
+# &sort=(0, 1, 2 : 관련순, 최신, 오래된 순)
+# &ds=(YYYY.MM.DD : 검색 시작)
+# &de=(YYYY.MM.DD : 검색 끝)"
 
-                # file.write(data+","+str(i[2]+"\n"))
-            # csvfile.writerow(t+","+str(session[0]))
-            resultList = list(set(resultList))  # 중복 제거
-            for result in resultList:
-                file.write(result[0] + "," + result[1] + "\n")
+res = req.get(url)
+soup_result = soup.BeautifulSoup(res.text, 'html.parser')
 
-            resultList = []
-            # 다 쓰고 나선 다음 페이지를 위해 resultList를 비워준다.
-            # 안그러면 같은 내용을 write한다.
+test = soup_result.find(class_="type01").find_all('li')
+for item in test :
+    testa = item.find('a')
+    print(item)
+# print(soup_result.find(class_="news mynews section _prs_nws").text)
